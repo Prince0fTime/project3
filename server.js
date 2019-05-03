68746b804e3e353befc13a2a86ef93c7ad6780c5
Mark Gardner <mark.gardner@endurance.com>
2019-05-02 20:46:44 -0500

    start api routes and controllers with adaptation from 20-react/01-Activities/10-Stu_AJAXFormDelete/Solved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
 
 // Define middleware here
@@ -12,12 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
