c4f9dcddc0f4690684fa725540db42e04eb08175
Mark Gardner <mark.gardner@endurance.com>
2019-05-09 19:43:22 -0500

    prettier

--- client/src/components/Card/Form.js
+++ client/src/components/Card/Form.js
@@ -1,57 +1,58 @@
-import React, { Component } from 'react';
-import AceEditor from 'react-ace';
-import '../../index.css';
-import 'brace/mode/javascript';
-import 'brace/theme/monokai';
+import React, { Component } from "react";
+import AceEditor from "react-ace";
+import "../../index.css";
+import "brace/mode/javascript";
+import "brace/theme/monokai";
 
 class Form extends Component {
   constructor() {
     super();
     this.state = {
-        title: '',
-        description: '',
-        content: '',
-        date: '',
-    }
+      title: "",
+      description: "",
+      content: "",
+      date: ""
+    };
   }
 
-  changeHandler = ({target}) => {
+  changeHandler = ({ target }) => {
     this.setState({
       [target.name]: target.value
     });
   };
 
-  onAceEditorChange = (value) => {
+  onAceEditorChange = value => {
     this.setState({
-        content: value
-      }
-    );
+      content: value
+    });
   };
 
   render() {
     return (
       <form>
         <input
-          type='string'
-          name='title'
+          id="title"
+          type="string"
+          name="title"
           value={this.state.title}
           onChange={this.changeHandler}
         />
         <AceEditor
-          mode='javascript'
-          theme='monokai'
-          name='content'
+          mode="javascript"
+          theme="monokai"
+          name="content"
           value={this.state.content}
           onChange={this.onAceEditorChange}
           editorProps={{ $blockScrolling: true }}
         />
         <input
-          type='string'
-          name='description'
+          id="description"
+          type="string"
+          name="description"
           defaultValue={this.state.description}
           onChange={this.changeHandler}
         />
-        <button type='submit' name='submit' />
+        <button type="submit" name="submit" />
       </form>
     );
   }
